0b06993363c2f63d8d6581b451401f4bd8475529
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-01-18 16:47:59 -0500

    using grunt-bump for release, fixes #11

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,27 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: grunt.file.readJSON('jshint.json'),
-    complexity: grunt.file.readJSON('complexity.json')
+    complexity: grunt.file.readJSON('complexity.json'),
+    /* to bump version, then run grunt (to update readme), then commit
+    grunt release
+    */
+    bump: {
+      options: {
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['-a'], // '-a' for all files
+        createTag: true,
+        tagName: '%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin'
+      }
+    }
   });
 
   var plugins = require('matchdep').filterDev('grunt-*');
   plugins.forEach(grunt.loadNpmTasks);
 
   grunt.registerTask('default', ['jshint', 'complexity']);
+  grunt.registerTask('release', ['bump-only:patch', 'bump-commit']);
 };
