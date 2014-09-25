2857c52cf6d20c79c268681c2051512531e948d0
Vlad Kotushenko <volodymyr_kotushenko@epam.com>
2014-09-25 13:39:01 +0300

    detect current branch for prepush commits detection

--- hooks/pre-push.js
+++ hooks/pre-push.js
@@ -8,22 +8,30 @@ var label = 'pre-push';
 
 function haveCommitsToPush(cb) {
   // todo: find if there are commits to push for any remote name / any branch
-  child.exec('git log origin/master..HEAD', function (err, stdout) {
-    if (err) {
-      console.error(label, 'Failed to check for commits. Cannot run the tests.');
-      console.error(label, err);
-      return process.exit(1);
+  // detect current branch
+  child.exec('git rev-parse --abbrev-ref HEAD', function (err, stdout) {
+    var branch = 'master'
+    if (stdout.trim().length) {
+      branch = stdout.trim()
     }
+    child.exec('git log origin/' + branch + '..HEAD', function (err, stdout) {
+      if (err) {
+        console.error(label, 'Failed to check for commits. Cannot run the tests.');
+        console.error(label, err);
+        return process.exit(1);
+      }
 
-    if (!stdout.trim().length) {
-      console.log('');
-      console.log(label, 'No local commits detected, bailing out.');
-      console.log('');
-      return process.exit(0);
-    }
-
-    cb();
-  });
+      if (!stdout.trim().length) {
+        console.log('');
+        console.log(label, 'No local commits detected, bailing out.');
+        console.log('');
+        return process.exit(0);
+      }
+      console.log(label, "Detected commits:");
+      console.log(label, stdout.trim());
+      cb();
+    });
+  })
 }
 
 run(label, haveCommitsToPush);
