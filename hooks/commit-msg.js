f7bd4ac85f0a1649573f165535306d2e4bb7a2e0
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-11-06 12:22:23 -0500

    commit message will just run the commands

--- hooks/commit-msg.js
+++ hooks/commit-msg.js
@@ -4,16 +4,19 @@
 
 var run = require(__dirname + '/pre-common');
 var label = 'commit-msg';
-var isValid = require('validate-commit-message');
+
+// var isValid = require('validate-commit-message');
 
 function validateCommitMessage(cb) {
   console.log('commit-msg');
+  /*
   if (isValid()) {
     console.log('commit message is valid');
     cb();
   } else {
     console.log('commit message is invalid');
-  }
+  }*/
+  cb();
 }
 
 run(label, validateCommitMessage);
