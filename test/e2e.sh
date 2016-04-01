68930273080f8c9e388f2c5e3d82b2366b88d2f4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-01 11:05:48 -0400

    chore(test): added e2e shell script for simple test, closes #80

---
+++ test/e2e.sh
@@ -0,0 +1,28 @@
+echo "Linking current pre-git"
+npm link
+
+echo "Creating test folder"
+folder=/tmp/test-pre-git
+rm -rf $folder
+mkdir $folder
+cd $folder
+echo "Created test folder $folder"
+
+echo "Creating local git repo"
+git init
+
+echo "Running npm init -y"
+npm init --yes
+echo "started package"
+
+echo "Installing git hooks"
+npm install --save-dev pre-git
+echo "Package.json after installing pre-git"
+cat package.json
+git add package.json
+git commit -m "chore(test): this is a test commit"
+
+ls -la
+git log --oneline
+git show
+echo "All done testing pre-git"
