3907be8ade3f0972b20976efdb154d4d8470297f
jdiaz <jdiaz@alertlogic.com>
2016-06-24 15:02:49 -0500

    Added feature for checking commit message against a given regex configured in the package.json, this can be used with the configured commit wizard as it is non blocker, example: 'msg-pattern': '^(US|DE)\d+ - .+'

--- bin/commit-msg.js
+++ bin/commit-msg.js
@@ -8,20 +8,40 @@ const la = require('lazy-ass');
 const check = require('check-more-types');
 const log = require('debug')('pre-git');
 
-const wizard = preGit.wizard();
-if (!wizard) {
-  log('no commit message wizard defined');
-  process.exit(0);
+function checkMessageAgainstPattern(msg, pattern) {
+
+  var regex = new RegExp(pattern);
+  
+  if (!regex.test(msg)) {
+    log('invalid commit message, must match the following pattern: ' + pattern, msg);
+    process.exit(-1);
+  }
+
+  return true;
+
 }
 
-log('found commit message wizard with name', wizard.name);
-
-la(check.fn(wizard.validate),
-  'missing wizard validate method,', Object.keys(wizard));
-la(check.fn(preGit.printError),
-  'missing preGit.printError,', Object.keys(preGit));
-
 function checkMessage(msg) {
+
+  const msgPattern = preGit.customMsgPattern();
+  
+  if (msgPattern) {
+    checkMessageAgainstPattern(msg, msgPattern);
+  }
+
+  const wizard = preGit.wizard();
+  if (!wizard) {
+    log('no commit message wizard defined');
+    process.exit(0);
+  }
+
+  log('found commit message wizard with name', wizard.name);
+
+  la(check.fn(wizard.validate),
+      'missing wizard validate method,', Object.keys(wizard));
+  la(check.fn(preGit.printError),
+      'missing preGit.printError,', Object.keys(preGit));
+
   log('checking commit message:', msg);
   const isValid = wizard.validate(msg);
   if (!isValid) {
